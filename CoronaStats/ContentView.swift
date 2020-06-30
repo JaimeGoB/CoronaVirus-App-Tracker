52c364c6bed9dc0c906c7c752efd6b1f08a0e737
JaimeGoB <josba93@gmail.com>
2020-06-30 11:13:30 -0500

    Initial Commit

---
+++ CoronaStats/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoronaStats
+//
+//  Created by sj on 6/30/20.
+//  Copyright © 2020 sj. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
